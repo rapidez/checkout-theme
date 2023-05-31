0ea5fd32ee82178389bba09a6825d6b590d201b2
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-05-31 10:11:38 +0200

    minimalized coupon

--- resources/views/cart/coupon.blade.php
+++ resources/views/cart/coupon.blade.php
@@ -1,42 +1,41 @@
 <toggler>
     <div slot-scope="{ isOpen, toggle }">
-        <div
-            class="w-full sm:w-auto"
+        <coupon
             v-show="isOpen"
+            v-slot="{ cart, removeCoupon, couponCode, inputEvents, applyCoupon, submitError }"
         >
-            <coupon v-slot="{ cart, removeCoupon, couponCode, inputEvents, applyCoupon, submitError }">
-                <form @submit.prevent="applyCoupon">
-                    <div class="flex w-full flex-row gap-x-3">
-                        <x-rapidez-ct::input
-                            class="h-full text-ct-primary"
-                            name="couponCode"
-                            :placeholder="__('Enter code') . '...'"
-                            v-on="inputEvents"
-                            v-bind:value="couponCode"
-                            v-bind:disabled="$root.loading"
-                        ></x-rapidez-ct::input>
-                        <x-rapidez-ct::button.outline type="submit">@lang('Apply')</x-rapidez-ct::button.outline>
-                    </div>
-                    <div class="mt-1 flex flex-col text-sm">
-                        <div
-                            class="flex items-center gap-x-2 text-ct-inactive"
-                            v-if="cart.discount_name && cart.discount_amount < 0"
-                        >
-                            <button v-on:click="removeCoupon">
-                                <x-heroicon-s-x class="h-4 w-4" />
-                            </button>
-                            @lang('Discount'): @{{ cart.discount_name }}
-                        </div>
-                        <div
-                            class="mt-1 italic text-ct-error"
-                            v-if="submitError"
-                        >
-                            @{{ submitError }}
-                        </div>
-                    </div>
-                </form>
-            </coupon>
-        </div>
+            <form
+                class="text-sm"
+                @submit.prevent="applyCoupon"
+            >
+                <div class="flex w-full flex-row gap-x-3">
+                    <x-rapidez-ct::input
+                        class="text-ct-primary h-full"
+                        name="couponCode"
+                        :placeholder="__('Enter code') . '...'"
+                        v-on="inputEvents"
+                        v-bind:value="couponCode"
+                        v-bind:disabled="$root.loading"
+                    ></x-rapidez-ct::input>
+                    <x-rapidez-ct::button.outline type="submit">@lang('Apply')</x-rapidez-ct::button.outline>
+                </div>
+                <div
+                    class="text-ct-inactive mt-1 flex items-center gap-x-2"
+                    v-if="cart.discount_name && cart.discount_amount < 0"
+                >
+                    <button v-on:click="removeCoupon">
+                        <x-heroicon-s-x class="h-4 w-4" />
+                    </button>
+                    @lang('Discount'): @{{ cart.discount_name }}
+                </div>
+                <div
+                    class="text-ct-error mt-1 italic"
+                    v-if="submitError"
+                >
+                    @{{ submitError }}
+                </div>
+            </form>
+        </coupon>
         <x-rapidez-ct::button.outline
             v-show="!isOpen"
             @click="toggle"
