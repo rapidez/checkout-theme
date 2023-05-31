4adadc60826ba541617a5345b91350f01616c050
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-05-31 14:47:36 +0200

    add @lang

--- resources/views/components/input/index.blade.php
+++ resources/views/components/input/index.blade.php
@@ -2,7 +2,7 @@
 <label {{ $attributes->only(['v-if', 'v-else', 'v-else-if', 'class'])->merge(['class' => 'relative flex flex-col gap-y-2 text-sm']) }}>
     @if (isset($label) && $label)
         <div class="flex">
-            <span class="text-inactive">{{ $label }}</span>
+            <span class="text-inactive">@lang($label)</span>
             @if ($attributes['required'])
                 <span>*</span>
             @endif
