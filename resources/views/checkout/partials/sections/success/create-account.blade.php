7b5fe1584e8f1d9981b92214f5025647abad644e
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-06-08 12:49:08 +0200

    Add create account in the checkout success & password input styling

--- resources/views/checkout/partials/sections/success/create-account.blade.php
+++ resources/views/checkout/partials/sections/success/create-account.blade.php
@@ -2,11 +2,41 @@
     <x-rapidez-ct::title.lg>
         @lang('Create account')
     </x-rapidez-ct::title.lg>
-    <x-rapidez-ct::input
-        name="email"
-        type="email"
-        label="E-mailaddress"
-        v-bind:value="email"
-        disabled
-    />
+    <graphql-mutation
+        v-cloak
+        query="mutation customer ($firstname: String!, $lastname: String!, $email: String!, $password: String) { createCustomerV2 ( input: { firstname: $firstname, lastname: $lastname, email: $email, password: $password } ) { customer { email } } }"
+        redirect="/account"
+        :variables="{
+            firstname: order.customer_firstname,
+            lastname: order.customer_lastname,
+            email: order.customer_email
+        }"
+        :callback="registerCallback"
+    >
+        <form
+            class="mt-5 grid grid-cols-2 items-end gap-5"
+            slot-scope="{ mutate, variables, mutating }"
+            v-on:submit="mutate"
+        >
+            <x-rapidez-ct::input
+                name="email"
+                type="email"
+                label="E-mailaddress"
+                v-bind:value="order.customer_email"
+                disabled
+            />
+            <div class="text-sm text-ct-primary">
+                @lang('No account yet? Create an account and benefit instantly from repeat orders, order statuses and easy returns!')
+            </div>
+            <x-rapidez-ct::input.password
+                name="password"
+                label="Password"
+                v-model="variables.password"
+                required
+            />
+            <x-rapidez-ct::button.accent class="justify-self-start mt-auto">
+                @lang('Create account')
+            </x-rapidez-ct::button.accent>
+        </form>
+    </graphql-mutation>
 </section>
---
+++ resources/views/components/input/password.blade.php
@@ -0,0 +1,29 @@
+<toggler>
+    <div
+        class="relative"
+        slot-scope="{ isOpen, toggle, close }"
+    >
+        <x-rapidez-ct::input
+            class="[&>input]:pr-12"
+            type="password"
+            v-bind:type="isOpen ? 'text' : 'password'"
+            label="password"
+            {{ $attributes }}
+        />
+        @if (!$attributes['disabled'] ?? false)
+            <div
+                class="absolute right-5 bottom-4 cursor-pointer"
+                v-on:click="toggle"
+            >
+                <x-heroicon-o-eye
+                    class="h-5"
+                    v-if="isOpen"
+                />
+                <x-heroicon-o-eye-off
+                    class="h-5"
+                    v-else
+                />
+            </div>
+        @endif
+    </div>
+</toggler>
