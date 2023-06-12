16c0f43618d39df549c41d927a8eb4b5d8f73d3b
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-06-12 09:29:30 +0200

    Update cards

--- resources/views/checkout/partials/sidebar/user-info.blade.php
+++ resources/views/checkout/partials/sidebar/user-info.blade.php
@@ -1,27 +1,27 @@
 <template v-if="checkout.step < 4">
     <template v-if="checkout.hide_billing || checkout.shipping_address?.customer_address_id == checkout.billing_address?.customer_address_id">
         <x-rapidez-ct::card>
-            <x-rapidez-ct::card.address v-bind:address="checkout.shipping_address" shipping billing>
+            <x-rapidez-ct::address v-bind:address="checkout.shipping_address" shipping billing>
                 <x-rapidez-ct::button.link v-if="checkout.step == 3" v-on:click="goToStep(2)">
                     @lang('Edit')
                 </x-rapidez-ct::button.link>
-            </x-rapidez-ct::card.address>
+            </x-rapidez-ct::address>
         </x-rapidez-ct::card>
     </template>
     <template v-else>
         <x-rapidez-ct::card>
-            <x-rapidez-ct::card.address v-bind:address="checkout.shipping_address" shipping>
+            <x-rapidez-ct::address v-bind:address="checkout.shipping_address" shipping>
                 <x-rapidez-ct::button.link v-if="checkout.step == 3" v-on:click="goToStep(2)">
                     @lang('Edit')
                 </x-rapidez-ct::button.link>
-            </x-rapidez-ct::card.address>
+            </x-rapidez-ct::address>
         </x-rapidez-ct::card>
         <x-rapidez-ct::card>
-            <x-rapidez-ct::card.address v-bind:address="checkout.billing_address" billing>
+            <x-rapidez-ct::address v-bind:address="checkout.billing_address" billing>
                 <x-rapidez-ct::button.link v-if="checkout.step == 3" v-on:click="goToStep(2)">
                     @lang('Edit')
                 </x-rapidez-ct::button.link>
-            </x-rapidez-ct::card.address>
+            </x-rapidez-ct::address>
         </x-rapidez-ct::card>
     </template>
 </template>
