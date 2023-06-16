49fc76f80bdeffa7ae22e6864c186ad83f239aac
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-06-16 09:27:14 +0200

    add cursor-pointer

--- resources/views/components/input/checkbox.blade.php
+++ resources/views/components/input/checkbox.blade.php
@@ -4,7 +4,7 @@
     {{ $attributes->only('class')->class('text-ct-primary relative flex cursor-pointer select-none flex-wrap gap-x-3 text-sm [&>span]:flex-1') }}
 >
     <input
-        class="text-ct-accent peer h-6 w-6 rounded border-0 transition focus:outline-none focus:ring-0 focus:ring-offset-0"
+        class="text-ct-accent peer h-6 w-6 cursor-pointer rounded border-0 transition focus:outline-none focus:ring-0 focus:ring-offset-0"
         id="{{ $id }}"
         name="{{ $name }}"
         type="checkbox"
