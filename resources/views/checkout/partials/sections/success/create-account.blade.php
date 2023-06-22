1d22b79416545b081f0b1557cea8312bf4a62351
Jade Geels <jade@justbetter.nl>
2023-06-22 08:39:08 +0200

    Fix account registration on success page

--- resources/views/checkout/partials/sections/success/create-account.blade.php
+++ resources/views/checkout/partials/sections/success/create-account.blade.php
@@ -5,18 +5,18 @@
     <graphql-mutation
         v-cloak
         query="mutation customer ($firstname: String!, $lastname: String!, $email: String!, $password: String) { createCustomerV2 ( input: { firstname: $firstname, lastname: $lastname, email: $email, password: $password } ) { customer { email } } }"
-        redirect="/account"
         :variables="{
             firstname: order.customer_firstname,
             lastname: order.customer_lastname,
             email: order.customer_email
         }"
         :callback="registerCallback"
+        :notify="{ message: '@lang('Account registration successful.')' }"
     >
         <form
             class="mt-5 grid items-end gap-5 sm:grid-cols-2"
             slot-scope="{ mutate, variables, mutating }"
-            v-on:submit="mutate"
+            v-on:submit.prevent="mutate"
         >
             <x-rapidez-ct::input
                 name="email"
