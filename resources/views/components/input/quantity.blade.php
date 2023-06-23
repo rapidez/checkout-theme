d8efb2c03666aac669e87ac6d5f2632bbea6d036
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-06-23 15:55:05 +0200

    add logic that disables quantity going in negative values

--- resources/views/components/input/quantity.blade.php
+++ resources/views/components/input/quantity.blade.php
@@ -1,7 +1,7 @@
 <div class="flex w-20 overflow-hidden rounded border">
     <button
         class="flex-1 bg-ct-inactive-100 transition hover:bg-opacity-80"
-        v-on:click="item.qty = +item.qty - item.qty_increments;changeQty(item)"
+        v-on:click="item.qty <= item.min_sale_qty ? item.qty = item.qty : item.qty = +item.qty - item.qty_increments;changeQty(item)"
     >-</button>
     <input
         class="h-10 w-1/2 border-none px-0 text-center text-sm [appearance:textfield] focus:ring-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
