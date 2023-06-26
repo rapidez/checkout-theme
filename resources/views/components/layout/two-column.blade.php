e17977d4dc1e5731283a7a097090978d1f1efdc9
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-06-26 11:56:10 +0200

    Capital P and made two collumns smaller

--- resources/views/components/layout/two-column.blade.php
+++ resources/views/components/layout/two-column.blade.php
@@ -1,4 +1,4 @@
-<div {{ $attributes->class('flex flex-col mx-auto max-w-5xl w-full') }}>
+<div {{ $attributes->class('flex flex-col mx-auto max-w-4xl w-full') }}>
     {{ $slot }}
     <div class="mt-5 grid content-center items-start gap-8 sm:grid-cols-2">
         {{ $columns }}
