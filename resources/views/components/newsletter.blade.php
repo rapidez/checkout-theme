054d256c8a9a495e331bd4939b0eddef7f7a2678
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-06-27 13:23:15 +0200

    Include absolute div inside newsletter visual

--- resources/views/components/newsletter.blade.php
+++ resources/views/components/newsletter.blade.php
@@ -12,11 +12,9 @@
             id="{{ $id }}"
         >
             <x-slot:slot class="ml-2 flex flex-col gap-1">
-                <span class="text-sm font-medium text-ct-primary">@lang('Yes, I want to subscribe to the newsletter')</span>
-                <span class="text-xs font-normal text-ct-inactive">@lang('You will receive this newsletter approximately 2x a year')</span>
-                <div class="absolute right-0 top-1/2 -translate-y-1/2 max-sm:hidden">
-                    <x-rapidez-ct::newsletter-visual />
-                </div>
+                <span class="text-ct-primary text-sm font-medium">@lang('Yes, I want to subscribe to the newsletter')</span>
+                <span class="text-ct-inactive text-xs font-normal">@lang('You will receive this newsletter approximately 2x a year')</span>
+                <x-rapidez-ct::newsletter-visual />
             </x-slot:slot>
         </x-rapidez-ct::input.checkbox>
     </label>
