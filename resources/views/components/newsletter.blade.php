73cb96bb499a4ab052a14c4d9ccb2e0001440c41
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-06-27 16:10:30 +0200

    fix label around newsletter

--- resources/views/components/newsletter.blade.php
+++ resources/views/components/newsletter.blade.php
@@ -14,23 +14,18 @@
             v-slot="{ mutate, variables }"
         >
     @endif
-    <label
-        for="{{ $id }}"
-        {{ $attributes->class('flex bg-white p-7 cursor-pointer relative rounded border') }}
+    <x-rapidez-ct::input.checkbox
+        class="relative flex w-full cursor-pointer !items-start rounded border bg-white p-7"
+        id="{{ $id }}"
+        v-model="{{ $vModel ?? 'variables.is_subscribed' }}"
+        v-on:change="{{ isset($vModel) ?: 'mutate();$root.user.extension_attributes.is_subscribed=variables.is_subscribed' }}"
     >
-        <x-rapidez-ct::input.checkbox
-            class="w-full !items-start"
-            id="{{ $id }}"
-            v-model="{{ $vModel ?? 'variables.is_subscribed' }}"
-            v-on:change="{{ isset($vModel) ?: 'mutate();$root.user.extension_attributes.is_subscribed=variables.is_subscribed' }}"
-        >
-            <x-slot:slot class="ml-2 flex flex-col gap-1">
-                <span class="text-ct-primary text-sm font-medium">@lang('Yes, I want to subscribe to the newsletter')</span>
-                <span class="text-ct-inactive text-xs font-normal">@lang('You will receive this newsletter approximately 2x a year')</span>
-                <x-rapidez-ct::newsletter-visual />
-            </x-slot:slot>
-        </x-rapidez-ct::input.checkbox>
-    </label>
+        <x-slot:slot class="ml-2 flex flex-col gap-1">
+            <span class="text-ct-primary text-sm font-medium">@lang('Yes, I want to subscribe to the newsletter')</span>
+            <span class="text-ct-inactive text-xs font-normal">@lang('You will receive this newsletter approximately 2x a year')</span>
+            <x-rapidez-ct::newsletter-visual />
+        </x-slot:slot>
+    </x-rapidez-ct::input.checkbox>
     @if (!isset($vModel))
         </graphql-mutation>
     @endif
