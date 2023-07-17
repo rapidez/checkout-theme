9d215ec2e90e70031daf3608237cf25baea2bea5
Jimmy <101801301+JimmyHoenderdaal@users.noreply.github.com>
2023-07-17 15:06:28 +0200

    Update resources/views/components/button/index.blade.php
    Co-authored-by: indykoning <15870933+indykoning@users.noreply.github.com>

--- resources/views/components/button/index.blade.php
+++ resources/views/components/button/index.blade.php
@@ -1,6 +1,9 @@
 <component
     is="{{ $component ?? ($attributes->has('href') || $attributes->has('v-bind:href') ? 'a' : 'button') }}"
-    {{ $attributes->class('inline-block self-start rounded py-4 px-6 text-center text-sm transition disabled:cursor-not-allowed disabled:opacity-70') }}
+    {{ $attributes->class([
+        'inline-block self-start rounded py-4 px-6 text-center text-sm transition',
+        'disabled:cursor-not-allowed disabled:opacity-70',
+    ]) }}
     v-bind:disabled="$root.loading"
 >
     {{ $slot }}
