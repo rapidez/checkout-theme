574f0ec1fe8239a0b3896e115d04375b2cdf059e
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-07-25 11:57:35 +0200

    frontend changes

--- resources/views/account/partials/address-form.blade.php
+++ resources/views/account/partials/address-form.blade.php
@@ -1,22 +1,32 @@
-<form slot-scope="{ variables, mutate, mutated }" v-on:submit.prevent="mutate">
+<form
+    slot-scope="{ variables, mutate, mutated }"
+    v-on:submit.prevent="mutate"
+>
     <x-rapidez-ct::card.inactive class="mb-6">
-        <x-rapidez-ct::address-form type="edit" address="variables" country-key="country_code"/>
+        <x-rapidez-ct::address-form
+            type="edit"
+            address="variables"
+            country-key="country_code"
+        />
 
-        <div class="flex py-5 gap-5">
+        <div class="flex gap-5 py-5">
             <x-rapidez-ct::input.checkbox v-model="variables.default_shipping">@lang('Default shipping address')</x-input.checkbox>
-            <x-rapidez-ct::input.checkbox v-model="variables.default_billing">@lang('Default billing address')</x-input.checkbox>
+                <x-rapidez-ct::input.checkbox v-model="variables.default_billing">@lang('Default billing address')</x-input.checkbox>
         </div>
 
     </x-rapidez-ct::card.inactive>
     <x-rapidez-ct::toolbar>
-        <x-rapidez-ct::button.outline class="w-full flex-1 sm:w-auto sm:flex-none" :href="route('account.edit')">
+        <x-rapidez-ct::button.outline :href="route('account.edit')">
             @lang('Back to settings')
         </x-rapidez-ct::button.outline>
-        <div class="mb-4 w-full sm:mb-0 sm:w-auto">
-            <x-rapidez-ct::button.accent class="w-full flex-1 sm:w-auto sm:flex-none" type="submit">
+        <div>
+            <x-rapidez-ct::button.accent type="submit">
                 @lang(request()->id ? 'Change address and save' : 'Add address and save')
             </x-rapidez-ct::button.accent>
-            <div v-if="mutated" class="text-green-500 ml-3">
+            <div
+                class="ml-3 text-green-500"
+                v-if="mutated"
+            >
                 @lang('Changed successfully!')
             </div>
         </div>
--- resources/views/checkout/partials/address-cards.blade.php
+++ resources/views/checkout/partials/address-cards.blade.php
@@ -5,7 +5,7 @@
                 @lang('Edit')
             </x-rapidez-ct::button.link>
         </x-rapidez-ct::card.address>
-        <button v-on:click.prevent="toggleEdit" class="h-full flex flex-col items-center justify-center gap-y-2 font-medium bg-ct-disabled rounded max-sm:hidden">
+        <button v-on:click.prevent="toggleEdit" class="flex flex-col items-center justify-center gap-y-2 font-medium bg-ct-disabled rounded max-sm:hidden">
             <span>+</span>
             <span>@lang('Add new address')</span>
         </button>
