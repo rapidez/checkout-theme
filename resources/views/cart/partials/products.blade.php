005d1f62e6f00f6c0cdf3a4948cc19e455c05003
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-07-26 13:54:02 +0200

    remove pixels from products

--- resources/views/cart/partials/products.blade.php
+++ resources/views/cart/partials/products.blade.php
@@ -1,42 +1,55 @@
-<div class="mt-5 rounded bg-ct-inactive-100 py-4 text-sm">
+<div class="bg-ct-inactive-100 mt-5 rounded py-4 text-sm">
     <div class="flex w-full items-center gap-x-6 pr-6 text-xs">
-        <div class="sm:w-[150px]"></div>
-        <div class="flex-1 sm:w-[150px]">@lang('Product')</div>
-        <div class="ml-auto flex items-center gap-10 max-md:hidden">
-            <div class="w-[60px]">@lang('Price')</div>
-            <div class="w-20 text-center">@lang('Amount')</div>
-            <div class="w-[60px]">@lang('Subtotal')</div>
+        <div class="sm:w-40"></div>
+        <div class="sm:w-40">@lang('Product')</div>
+        <div class="ml-auto flex items-center gap-10 max-sm:hidden">
+            <div class="w-16">@lang('Price')</div>
+            <div class="w-32 text-center">@lang('Amount')</div>
+            <div class="w-16">@lang('Subtotal')</div>
         </div>
     </div>
 </div>
 
 <ul>
-    <li v-for="(item, itemId, index) in cart.items" class="flex border-b py-5">
-        <div class="flex w-full flex-wrap gap-y-3 gap-x-3 sm:gap-x-6 sm:pr-6 text-sm md:items-center">
-            <div class="flex h-[100px] w-[150px] items-center justify-center">
+    <li
+        class="flex border-b py-5"
+        v-for="(item, itemId, index) in cart.items"
+    >
+        <div class="flex w-full flex-wrap gap-x-3 gap-y-3 text-sm sm:gap-x-6 sm:pr-6 md:items-center">
+            <div class="h-h-24 flex w-40 items-center justify-center">
                 <img
-                    class="max-h-[100px] max-w-[150px]"
+                    class="h-h-24 w-40"
                     :alt="item.name"
                     :src="'/storage/resizes/200/magento/catalog/product' + item.image + '.webp'"
                     height="100"
                     v-if="item.image"
                 >
-                <x-rapidez::no-image v-else class="h-[100px] w-[150px]"/>
+                <x-rapidez::no-image
+                    class="h-h-24 w-40"
+                    v-else
+                />
             </div>
-            <div class="flex w-[150px] flex-1 flex-col items-start">
-                <a :href="item.url | url" dusk="cart-item-name">@{{ item.name }}</a>
+            <div class="flex w-40 flex-col items-start sm:flex-1">
+                <a
+                    :href="item.url | url"
+                    dusk="cart-item-name"
+                >@{{ item.name }}</a>
                 <div v-for="(optionValue, option) in item.options">
                     @{{ option }}: @{{ optionValue }}
                 </div>
                 <div v-for="option in cart?.items2?.find((item) => item.item_id == itemId).options.filter((option) => !['info_buyRequest', 'option_ids'].includes(option.code))">
                     @{{ option.label }}: @{{ option.value.title || option.value }}
                 </div>
-                <button class="mt-1 text-xs text-ct-inactive hover:underline" @click="remove(item)" :dusk="'item-delete-' + index">
+                <button
+                    class="text-ct-inactive mt-1 text-xs hover:underline"
+                    @click="remove(item)"
+                    :dusk="'item-delete-' + index"
+                >
                     @lang('Remove')
                 </button>
             </div>
-            <div class="flex items-center gap-10 sm:ml-auto font-medium max-sm:flex-1 justify-between">
-                <div class="flex sm:w-[60px] flex-col gap-px text-sm">
+            <div class="flex items-center justify-between gap-10 font-medium max-sm:flex-1 sm:ml-auto">
+                <div class="flex flex-col gap-px text-sm sm:w-16">
                     <div v-if="item.specialPrice">
                         @{{ item.specialPrice | price }}
                     </div>
@@ -44,8 +57,10 @@
                         @{{ item.price | price }}
                     </div>
                 </div>
-                <x-rapidez-ct::input.quantity />
-                <div class="sm:w-[60px] text-sm">
+                <div class="flex w-32 items-center justify-center">
+                    <x-rapidez-ct::input.quantity />
+                </div>
+                <div class="text-sm sm:w-16">
                     @{{ item.total | price }}
                 </div>
             </div>
