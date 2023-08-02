111587655c66c79d6a152a60c1a9bca6dc1c4a1b
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-08-02 15:41:06 +0200

    revert translation on usps

--- resources/views/account/partials/account-features.blade.php
+++ resources/views/account/partials/account-features.blade.php
@@ -3,7 +3,7 @@
         @lang('Account features')
     </x-rapidez-ct::title.lg>
     <ul class="space-y-5 [&>*]:relative [&>*]:pl-7">
-        @foreach ([__('Track status of your order and view order history'), __('Quick and easy ordering without having to fill in your details every time'), __('Everything centralized in 1 place such as returns, exchanges and customer service')] as $feature)
+        @foreach (['Track status of your order and view order history', 'Quick and easy ordering without having to fill in your details every time', 'Everything centralized in 1 place such as returns, exchanges and customer service'] as $feature)
             <li>
                 <x-heroicon-o-check class="absolute left-0 top-0 h-5 text-ct-accent" />
                 <span>@lang($feature)</span>
