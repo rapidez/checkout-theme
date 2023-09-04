c893f7b08d5e15d44feb65026f1c97802323007f
Jade Geels <jade@justbetter.nl>
2023-09-04 09:25:34 +0200

    Fix title

--- resources/views/components/card/address.blade.php
+++ resources/views/components/card/address.blade.php
@@ -1,5 +1,7 @@
+@props(['customTitle' => null])
+
 <x-rapidez-ct::card.white {{ $attributes->only('v-if') }}>
-    <x-rapidez-ct::address {{ $attributes }}>
+    <x-rapidez-ct::address {{ $attributes }} :$customTitle>
         {{ $slot }}
     </x-rapidez-ct::address>
 </x-rapidez-ct::card.white>
