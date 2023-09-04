0220d8f6ae209ff0c3c5fcb1b198ef9a7ac2854b
Jade Geels <jade@justbetter.nl>
2023-09-04 10:50:29 +0200

    Add toggle for company & vat id fields (#32)

--- resources/views/account/partials/register-account.blade.php
+++ resources/views/account/partials/register-account.blade.php
@@ -1,6 +1,6 @@
 <x-rapidez::recaptcha location="customer_create"/>
 <graphql-mutation
-    query="mutation customer ($firstname: String!, $lastname: String!, $email: String!, $password: String) { createCustomerV2 ( input: { firstname: $firstname, lastname: $lastname, email: $email, password: $password } ) { customer { email } } }"
+    query="mutation customer ($firstname: String!, $lastname: String!, $email: String!, $password: String, $taxvat: String) { createCustomerV2 ( input: { firstname: $firstname, lastname: $lastname, email: $email, password: $password, taxvat: $taxvat } ) { customer { email } } }"
     redirect="{{ route('account.overview') }}"
     :callback="registerCallback"
     :recaptcha="{{ Rapidez::config('recaptcha_frontend/type_for/customer_create') == 'recaptcha_v3' ? 'true' : 'false' }}"
@@ -37,6 +37,29 @@
                 v-model="variables.password"
                 required
             />
+            @if(config('customer/create_account/vat_frontend_visibility', 0))
+                <toggler>
+                    <div slot-scope="{ toggle, isOpen }" class="contents">
+                        <x-rapidez-ct::input.checkbox
+                            id="isb2b"
+                            name="isb2b"
+                            v-model="isOpen"
+                            v-on:click="toggle"
+                        >
+                            @lang('This is a business account')
+                        </x-rapidez-ct::input.checkbox>
+                        <x-rapidez-ct::input
+                            v-cloak
+                            v-if="isOpen"
+                            name="taxvat"
+                            label="Vat ID"
+                            type="text"
+                            v-model="variables.taxvat"
+                            required
+                        />
+                        </div>
+                    </toggler>
+                @endif
         </form>
     </x-rapidez-ct::card.inactive>
 </graphql-mutation>
