8e869c245ef87e4a71cb79275fc3eb1ddb7d4db7
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-09-08 08:22:36 +0200

    add name to radio

--- resources/views/components/input/radio.blade.php
+++ resources/views/components/input/radio.blade.php
@@ -1,5 +1,6 @@
+@props(['name'])
 <label {{ $attributes->only('class')->class("relative flex w-full cursor-pointer items-center justify-start gap-x-3 rounded border bg-white px-5 sm:px-7 py-7 text-sm text-ct-primary") }}>
-    <input type="radio" {{ $attributes->except('class') }} class="peer text-ct-accent w-6 h-6 focus:ring-offset-0 focus:ring-0 border-ct-border transition"/>
+    <input name="{{ $name }}" type="radio" {{ $attributes->except('class') }} class="peer text-ct-accent w-6 h-6 focus:ring-offset-0 focus:ring-0 border-ct-border transition"/>
     <div class="absolute -inset-y-px -left-px w-1 rounded-l bg-ct-accent opacity-0 transition-all peer-checked:opacity-100"></div>
     @isset($slot)
         <div class="flex w-full flex-wrap items-center justify-between gap-x-3">
