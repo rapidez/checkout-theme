94755a760cf331907da1a487d1b2d6ee6d8a842e
indykoning <15870933+indykoning@users.noreply.github.com>
2023-10-13 14:43:30 +0200

    Update config usages for new config (#40)

--- resources/views/components/progress-bar/index.blade.php
+++ resources/views/components/progress-bar/index.blade.php
@@ -1,12 +1,13 @@
 @props(['checkout' => true])
+@php($steps = config('rapidez.frontend.checkout_steps.' . config('rapidez.store_code'), config('rapidez.checkout_steps.' . config('rapidez.store_code'), config('rapidez.checkout_steps.default'))))
 <div class="flex items-center space-x-3.5 sm:space-x-[18px] text-xs" v-cloak>
     <span class="whitespace-nowrap font-medium text-ct-inactive">
         @lang('Step :step out of :total', [
             'step' => '@{{ checkout.step }}',
-            'total' => count(config('rapidez.checkout_steps.' . config('rapidez.store_code')) ?? config('rapidez.checkout_steps.default')) - 1,
+            'total' => count($steps) - 1,
         ])
     </span>
-    @foreach (array_slice(config('rapidez.checkout_steps.' . config('rapidez.store_code')) ?? config('rapidez.checkout_steps.default'), 0, -1) as $stepTitle)
+    @foreach (array_slice($steps, 0, -1) as $stepTitle)
         <div
             class="aspect-square w-3 rounded"
             v-on:click="goToStep({{ $stepTitle == 'Credentials' ? 3 : $loop->index }})"
