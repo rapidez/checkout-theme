98e099a94a790c02d14b3daa95bd0c3ed897ac43
Jade Geels <jade@justbetter.nl>
2023-10-19 14:16:36 +0200

    Update heroicons some more

--- resources/views/components/input/password.blade.php
+++ resources/views/components/input/password.blade.php
@@ -11,7 +11,7 @@
             @if (!$attributes['disabled'] ?? false)
                 <div v-on:click="toggle" class="absolute right-5 bottom-4 cursor-pointer">
                     <x-heroicon-o-eye v-if="isOpen" class="h-5" v-cloak/>
-                    <x-heroicon-o-eye-off v-else class="h-5"/>
+                    <x-heroicon-o-eye-slash v-else class="h-5"/>
                 </div>
             @endif
         </div>
