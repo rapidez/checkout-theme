3fb4e9f305cc4257602739e5cb57b57f6ed3182e
Jade Geels <jade@justbetter.nl>
2023-10-26 13:33:46 +0200

    Remove newsletter from success page

--- resources/views/checkout/steps/success.blade.php
+++ resources/views/checkout/steps/success.blade.php
@@ -9,7 +9,6 @@
         <x-rapidez-ct::sections>
             @include('rapidez-ct::checkout.partials.sections.success.order-info')
             @include('rapidez-ct::checkout.partials.sections.success.products')
-            @include('rapidez-ct::components.newsletter')
             @includeWhen(
                 config('rapidez-checkout-theme.checkout.success.register'),
                 'rapidez-ct::checkout.partials.sections.success.create-account'
