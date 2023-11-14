5f03b8d86b42cdee53160a7f778c1a59de94afb1
jordy <jordy@justbetter.nl>
2023-11-14 13:47:59 +0100

    reordered cart by using the component as they should and removing the table margin

--- resources/views/cart/partials/bottom-bar.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="mt-6 flex flex-wrap justify-between gap-3">
-    <x-rapidez-ct::button.outline href="/">
-        @lang('Continue shopping')
-    </x-rapidez-ct::button.outline>
-    @include('rapidez-ct::cart.coupon')
-</div>
