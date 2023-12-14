1c4cbf8d71e7748bc0dc1b1ccbb0b272237ed928
Jade Geels <jade@justbetter.nl>
2023-12-14 10:27:33 +0100

    Use set width

--- resources/views/checkout/partials/sections/payment/payment-methods.blade.php
+++ resources/views/checkout/partials/sections/payment/payment-methods.blade.php
@@ -7,7 +7,7 @@
 >
     <div>@{{ method.title }}</div>
     <img
-        class="max-h-10"
+        class="max-h-10 w-12"
         v-bind:alt="method.code"
         v-bind:src="`/payment-icons/${method.code}.svg`"
         onerror="this.onerror=null; this.src='/payment-icons/default.svg'"
