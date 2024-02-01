6eb0e60ef340abc6e693d9910464ee2446d3aa1c
jordy <jordy@justbetter.nl>
2024-02-01 09:34:26 +0100

    refactored templates by splitting it into partials

---
+++ resources/views/cart/partials/cart-title.blade.php
@@ -0,0 +1,3 @@
+<x-rapidez-ct::title-progress-bar>
+    @lang('Cart')
+</x-rapidez-ct::title-progress-bar>
---
+++ resources/views/cart/partials/coupon-field.blade.php
@@ -0,0 +1,15 @@
+<div class="flex w-full flex-row gap-x-3 checkout-theme">
+    <x-rapidez-ct::input
+        class="text-ct-primary w-60"
+        name="couponCode"
+        :placeholder="__('Enter code') . '...'"
+        v-on="inputEvents"
+        v-bind:value="couponCode"
+        v-bind:disabled="$root.loading"
+        required
+    />
+    <x-rapidez-ct::button.outline type="submit" loader>
+        @lang('Apply')
+    </x-rapidez-ct::button.outline>
+</div>
+
