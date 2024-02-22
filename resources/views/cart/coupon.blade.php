5e184d5c86a276cae4b81e5f62d2ae7aa818e5d5
indy koning <indy@justbetter.nl>
2024-02-22 16:32:48 +0100

    Rapidez v2 compatibility

--- resources/views/cart/coupon.blade.php
+++ resources/views/cart/coupon.blade.php
@@ -1,32 +1,64 @@
 <toggler>
     <div slot-scope="{ isOpen, toggle }">
-        <coupon v-show="isOpen" v-slot="{ cart, removeCoupon, couponCode, inputEvents, applyCoupon, submitError }">
-            <form class="text-sm" @submit.prevent="applyCoupon">
-                <div class="flex w-full flex-row gap-x-3">
-                    <x-rapidez-ct::input
+        <div v-if="isOpen" v-cloak class="text-sm">
+            <graphql-mutation
+                :query="'mutation ($cart_id: String!, $coupon_code: String!) { applyCouponToCart(input: { cart_id: $cart_id, coupon_code: $coupon_code }) { cart { ' + config.queries.cart + ' } } }'"
+                :variables="{ cart_id: mask, coupon_code: '' }"
+                :notify="{ message: config.translations.cart.coupon.applied }"
+                :clear="true"
+                :watch="false"
+                :callback="updateCart"
+                :error-callback="checkResponseForExpiredCart"
+                v-slot="{ mutate, variables }"
+            >
+                <form v-on:submit.prevent="mutate" class="flex w-full flex-row gap-x-3">
+                    <x-rapidez::input
+                        :label="false"
                         class="text-ct-primary w-60"
                         name="couponCode"
-                        :placeholder="__('Enter code') . '...'"
-                        v-on="inputEvents"
-                        v-bind:value="couponCode"
+                        placeholder="Coupon code"
+                        v-model="variables.coupon_code"
                         v-bind:disabled="$root.loading"
                         required
                     />
-                    <x-rapidez-ct::button.outline type="submit" loader>
+                    <x-rapidez::button type="submit" class="sm:text-sm">
                         @lang('Apply')
-                    </x-rapidez-ct::button.outline>
-                </div>
-                <div v-if="cart.discount_name && cart.discount_amount < 0" class="text-ct-inactive mt-1 flex items-center gap-x-2">
-                    <button v-on:click="removeCoupon">
-                        <x-heroicon-s-x-mark class="h-4 w-4" />
-                    </button>
-                    @lang('Discount'): @{{ cart.discount_name }}
-                </div>
-                <div v-if="submitError" class="text-ct-error mt-1 italic">
-                    @{{ submitError }}
-                </div>
-            </form>
-        </coupon>
+                    </x-rapidez::button>
+                </form>
+            </graphql-mutation>
+
+            <div class="flex w-full flex-row gap-x-3">
+                <x-rapidez-ct::input
+                    class="text-ct-primary w-60"
+                    name="couponCode"
+                    :placeholder="__('Enter code') . '...'"
+                    v-on="inputEvents"
+                    v-bind:value="couponCode"
+                    v-bind:disabled="$root.loading"
+                    required
+                />
+                <x-rapidez-ct::button.outline type="submit" loader>
+                    @lang('Apply')
+                </x-rapidez-ct::button.outline>
+            </div>
+
+            <template v-if="cart.applied_coupons?.length" v-for="coupon in cart.applied_coupons" v-cloak>
+                <graphql-mutation
+                    :query="'mutation ($cart_id: String!) { removeCouponFromCart(input: { cart_id: $cart_id }) { cart { ' + config.queries.cart + ' } } }'"
+                    :variables="{ cart_id: mask }"
+                    :callback="updateCart"
+                    :error-callback="checkResponseForExpiredCart"
+                    v-slot="{ mutate }"
+                >
+                    <form v-on:submit.prevent="mutate" class="text-ct-inactive mt-1 flex items-center gap-x-2">
+                        <button type="submit">
+                            <x-heroicon-s-x-mark class="h-4 w-4 text-black-400"/>
+                        </button>
+                        @{{ coupon.code }}
+                    </form>
+                </graphql-mutation>
+            </template>
+        </div>
         <x-rapidez-ct::button.outline v-show="!isOpen" @click="toggle">
             @lang('Coupon code')
         </x-rapidez-ct::button.outline>
--- resources/views/cart/overview.blade.php
+++ resources/views/cart/overview.blade.php
@@ -5,21 +5,27 @@
 @section('robots', 'NOINDEX,NOFOLLOW')
 
 @section('content')
-    <cart v-cloak>
-        <div v-if="hasItems" class="container" slot-scope="{ cart, hasItems, changeQty, remove }">
-            <x-rapidez-ct::layout class="!mt-0">
-                @include('rapidez-ct::cart.cart')
-                <x-slot:sidebar>
-                    @include('rapidez-ct::cart.partials.sidebar.sidebar')
-                </x-slot:sidebar>
-            </x-rapidez-ct::layout>
-            @include('rapidez-ct::cart.partials.crosssells')
-        </div>
-        <div v-else class="container">
-            <p>@lang("You don't have anything in your cart.")</p>
-            <x-rapidez-ct::button.outline class="mt-3" href="/">
-                @lang('Return to home')
-            </x-rapidez-ct::button.outline>
-        </div>
-    </cart>
+    <graphql
+    v-if="mask"
+    :query="'query getCart($cart_id: String!) { cart (cart_id: $cart_id) { ' + config.queries.cart + ' } }'"
+    :variables="{ cart_id: mask }"
+    :callback="updateCart"
+    :error-callback="checkResponseForExpiredCart"
+    >
+    </graphql>
+    <div v-if="hasCart" v-cloak class="container">
+        <x-rapidez-ct::layout class="!mt-0">
+            @include('rapidez-ct::cart.cart')
+            <x-slot:sidebar>
+                @include('rapidez-ct::cart.partials.sidebar.sidebar')
+            </x-slot:sidebar>
+        </x-rapidez-ct::layout>
+        @include('rapidez-ct::cart.partials.crosssells')
+    </div>
+    <div v-else class="container">
+        <p>@lang("You don't have anything in your cart.")</p>
+        <x-rapidez-ct::button.outline class="mt-3" href="/">
+            @lang('Return to home')
+        </x-rapidez-ct::button.outline>
+    </div>
 @endsection
