e870b317ff73145eff288c4abafcc09c379470b8
indykoning <15870933+indykoning@users.noreply.github.com>
2024-03-13 11:52:51 +0100

    Rapidez v2 compatibility (#68)

--- resources/views/checkout/overview.blade.php
+++ resources/views/checkout/overview.blade.php
@@ -5,18 +5,18 @@
 @section('robots', 'NOINDEX,NOFOLLOW')
 
 @section('content')
-    <checkout v-slot="{ checkout, cart, hasItems, save, goToStep }" :set="checkout.step = Math.max(checkout.step, 2)" v-cloak>
+    <checkout v-slot="{ checkout, save, goToStep, forceAccount }" :set="checkout.step = Math.max(checkout.step, 2)" v-cloak>
         <div class="container">
             <x-rapidez-ct::layout class="mt-8 sm:mt-14">
-                <template v-if="checkout.step == 2 && hasItems">
+                <template v-if="checkout.step == 2 && $root?.cart?.total_quantity">
                     @include('rapidez-ct::checkout.steps.credentials')
                 </template>
 
-                <template v-if="checkout.step == 3">
+                <template v-if="checkout.step == getCheckoutStep('payment')">
                     @include('rapidez-ct::checkout.steps.payment')
                 </template>
 
-                <template v-if="checkout.step == 4">
+                <template v-if="checkout.step == getCheckoutStep('success')">
                     @include('rapidez-ct::checkout.steps.success')
                 </template>
 
--- resources/views/components/input/quantity.blade.php
+++ resources/views/components/input/quantity.blade.php
@@ -1,20 +1,31 @@
-<div class="flex w-20 overflow-hidden rounded border">
-    <button
-        class="flex-1 bg-ct-inactive-100 transition hover:bg-opacity-80"
-        v-on:click="item.qty <= item.min_sale_qty ? item.qty = item.qty : item.qty = +item.qty - item.qty_increments;changeQty(item)"
-    >-</button>
-    <input
-        class="h-10 w-2/5 border-none px-0 text-center text-sm [appearance:textfield] focus:ring-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-        name="qty"
-        type="number"
-        {{ $attributes }}
-        v-model="item.qty"
-        v-on:change="changeQty(item)"
-        v-bind:min="item.min_sale_qty > item.qty_increments ? item.min_sale_qty : item.qty_increments"
-        v-bind:step="item.qty_increments"
-    />
-    <button
-        class="flex-1 bg-ct-inactive-100 transition hover:bg-opacity-80"
-        v-on:click="item.qty = +item.qty + item.qty_increments;changeQty(item)"
-    >+</button>
-</div>
+
+<graphql-mutation
+    :query="'mutation ($cart_id: String!, $cart_item_id: Int, $quantity: Float) { updateCartItems(input: { cart_id: $cart_id, cart_items: [{ cart_item_id: $cart_item_id, quantity: $quantity }] }) { cart { ' + config.queries.cart + ' } } }'"
+    :variables="{ cart_id: mask, cart_item_id: item.id, quantity: item.quantity }"
+    :callback="updateCart"
+    :error-callback="checkResponseForExpiredCart"
+    v-slot="{ mutate, variables }"
+>
+    <form v-on:submit.prevent="mutate" class="flex w-20 overflow-hidden rounded border">
+        <button
+            class="flex-1 bg-ct-inactive-100 transition hover:bg-opacity-80"
+            v-on:click.prevent="variables.quantity <= (item.product.stock_item?.min_sale_qty || 1) ? variables.quantity = variables.quantity : variables.quantity = +variables.quantity - (item.product.stock_item?.qty_increments || 1);mutate()"
+        >-</button>
+        <input
+            class="h-10 w-2/5 border-none px-0 text-center text-sm [appearance:textfield] focus:ring-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+            name="qty"
+            type="number"
+            {{ $attributes }}
+            v-model="variables.quantity"
+            v-on:change="mutate"
+            v-bind:min="Math.max(item.product.stock_item?.min_sale_qty, item.product.stock_item?.qty_increments) || null"
+            v-bind:max="item.product.stock_item?.max_sale_qty"
+            v-bind:step="item.qty_increments"
+            v-bind:dusk="'qty-'+index"
+        />
+        <button
+            class="flex-1 bg-ct-inactive-100 transition hover:bg-opacity-80"
+            v-on:click.prevent="variables.quantity = +variables.quantity + (item.product.stock_item?.qty_increments || 1);mutate()"
+        >+</button>
+    </form>
+</graphql-mutation>
