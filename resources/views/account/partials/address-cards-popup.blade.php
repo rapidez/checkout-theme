3465015f6683e89a52200c7478d1fd99087f0fcf
Jade Geels <jade@justbetter.nl>
2024-04-11 12:52:58 +0200

    Small refactor

--- resources/views/account/partials/address-cards-popup.blade.php
+++ resources/views/account/partials/address-cards-popup.blade.php
@@ -14,27 +14,7 @@
                     check="userAddress.default_billing || userAddress.default_shipping"
                     class="w-full sm:min-w-[350px]"
                 >
-                    <x-rapidez-ct::button.link v-bind:href="`/account/address/${userAddress.id}`">
-                        @lang('Edit')
-                    </x-rapidez-ct::button.link>
-                    <x-rapidez-ct::button.link v-if="!userAddress.default_shipping" v-on:click="userAddress.default_shipping=true;mutate()">
-                        @lang('Select as shipping')
-                    </x-rapidez-ct::button.link>
-                    <x-rapidez-ct::button.link v-else v-on:click="userAddress.default_shipping=false;mutate()">
-                        @lang('Deselect as shipping')
-                    </x-rapidez-ct::button.link>
-
-                    <x-rapidez-ct::button.link v-if="!userAddress.default_billing" v-on:click="userAddress.default_billing=true;mutate()">
-                        @lang('Select as billing')
-                    </x-rapidez-ct::button.link>
-                    <x-rapidez-ct::button.link v-else v-on:click="userAddress.default_billing=false;mutate()">
-                        @lang('Deselect as billing')
-                    </x-rapidez-ct::button.link>
-                    <graphql-mutation query="mutation deleteCustomerAddress($id: Int!){ deleteCustomerAddress ( id: $id ) }" :variables="{ id: userAddress.id }" v-slot="{ mutate }">
-                        <x-rapidez-ct::button.link v-if="!userAddress.default_billing && !userAddress.default_shipping" v-on:click="data.customer.addresses.splice(addressIndex,1);mutate()">
-                            @lang('Delete address')
-                        </x-rapidez-ct::button.link>
-                    </graphql-mutation>
+                    @include('rapidez-ct::account.partials.address-cards.buttons')
                 </x-rapidez-ct::card.address>
                 </x-rapidez-ct::card.address>
             </graphql-mutation>
