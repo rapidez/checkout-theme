2bd75c6d4a42b16ff4d1555d5a0711c40c3b9db8
Jade Geels <jade@justbetter.nl>
2024-04-11 13:19:25 +0200

    Move delete button to top & disable default select buttons when mutating

--- resources/views/components/button/link.blade.php
+++ resources/views/components/button/link.blade.php
@@ -1,7 +1,7 @@
 @props(['tag'])
 <x-tag
     is="{{ $tag ?? ($attributes->has('href') || $attributes->has('v-bind:href') ? 'a' : 'button') }}"
-    {{ $attributes->merge(['class' => 'underline text-ct-inactive text-right']) }}
+    {{ $attributes->merge(['class' => 'underline text-ct-inactive text-right disabled:no-underline disabled:cursor-not-allowed disabled:opacity-50']) }}
 >
     {{ $slot }}
 </x-tag>
