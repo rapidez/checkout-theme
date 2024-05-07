1fc69fcb1937571c6ad0bfa2265229860cab8612
Jade Geels <jade@justbetter.nl>
2024-05-07 11:08:19 +0200

    Remove extra coupon field

--- resources/views/components/coupon-field.blade.php
+++ resources/views/components/coupon-field.blade.php
@@ -23,18 +23,3 @@
         </x-rapidez::button>
     </form>
 </graphql-mutation>
-
-<div class="flex w-full gap-x-3">
-    <x-rapidez-ct::input
-        class="text-ct-primary w-60"
-        name="couponCode"
-        :placeholder="__('Enter code') . '...'"
-        v-on="inputEvents"
-        v-bind:value="couponCode"
-        v-bind:disabled="$root.loading"
-        required
-    />
-    <x-rapidez-ct::button.outline type="submit" loader>
-        @lang('Apply')
-    </x-rapidez-ct::button.outline>
-</div>
