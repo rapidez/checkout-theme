e0ed4ef361e65150680e27d42a232f2d5fe38a95
Jade Geels <jade@justbetter.nl>
2024-05-10 11:33:37 +0200

    Update remove button to 2.x

--- resources/views/cart/partials/product/remove-button.blade.php
+++ resources/views/cart/partials/product/remove-button.blade.php
@@ -1,3 +1,12 @@
-<button v-on:click="remove(item)" class="text-ct-inactive mt-1 text-xs hover:underline" :dusk="'item-delete-' + index">
-    @lang('Remove')
-</button>
+<graphql-mutation
+    :query="'mutation ($cart_id: String!, $cart_item_id: Int) { removeItemFromCart(input: { cart_id: $cart_id, cart_item_id: $cart_item_id }) { cart { ' + config.queries.cart + ' } } }'"
+    :variables="{ cart_id: mask, cart_item_id: item.id }"
+    :notify="{ message: item.product.name+' '+config.translations.cart.remove }"
+    :callback="updateCart"
+    :error-callback="checkResponseForExpiredCart"
+    v-slot="{ mutate }"
+>
+    <button v-on:click="mutate" class="text-ct-inactive mt-1 text-xs hover:underline" :dusk="'item-delete-' + index">
+        @lang('Remove')
+    </button>
+</graphql-mutation>
