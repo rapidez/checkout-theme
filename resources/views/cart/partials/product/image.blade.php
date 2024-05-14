5148542aabc13ea8311791a25f6e0960ca5a618b
Jade Geels <jade@justbetter.nl>
2024-05-14 06:21:36 +0000

    Use correct item image check (#89)

--- resources/views/cart/partials/product/image.blade.php
+++ resources/views/cart/partials/product/image.blade.php
@@ -1,5 +1,5 @@
 <td class="h-24 w-40 !pl-0 max-md:flex max-md:w-1/2">
-    <img v-if="item.image" class="object-contain" :alt="item.product.name" :src="'/storage/{{ config('rapidez.store') }}/resizes/200/magento/catalog/product' + item.product.image.url.replace(config.media_url, '') + '.webp'">
+    <img v-if="item.product.image" class="object-contain" :alt="item.product.name" :src="'/storage/{{ config('rapidez.store') }}/resizes/200/magento/catalog/product' + item.product.image.url.replace(config.media_url, '') + '.webp'">
     <x-rapidez::no-image v-else />
 </td>
 
