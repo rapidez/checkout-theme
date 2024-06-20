ec8ae53117fc26037f9cf5c3e96402b3b2000bec
Jade Geels <jade@justbetter.nl>
2024-06-20 14:32:12 +0200

    Remove extra parentheses

--- resources/views/components/button/index.blade.php
+++ resources/views/components/button/index.blade.php
@@ -1,3 +1,3 @@
-<x-rapidez-ct::button.base {{ $attributes->class('py-4 px-6 rounded')) }}>
+<x-rapidez-ct::button.base {{ $attributes->class('py-4 px-6 rounded') }}>
     {{ $slot }}
 </x-rapidez-ct::button.base>
