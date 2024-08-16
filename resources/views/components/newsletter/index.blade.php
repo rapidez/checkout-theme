556c83ab8da12edffb794c2e48e2ea3b6d97a74a
Jade Geels <jade@justbetter.nl>
2024-08-16 08:36:36 +0200

    Newsletter on checkout success for logged in customers fix (#101)

--- resources/views/components/newsletter/index.blade.php
+++ resources/views/components/newsletter/index.blade.php
@@ -1,4 +1,4 @@
-@props(['id' => uniqId('newsletter-')])
+@props(['id' => uniqId('newsletter-'), 'hasData' => false])
 
 <x-rapidez-ct::card.inactive>
     <x-rapidez-ct::title.lg class="mb-5">
@@ -6,19 +6,23 @@
     </x-rapidez-ct::title.lg>
 
     @if (!$attributes->has('v-model'))
+        @php
+            $subscribedData = $hasData
+                ? 'data?.customer?.is_subscribed ?? $root.user?.extension_attributes?.is_subscribed ?? false'
+                : '$root.user?.extension_attributes?.is_subscribed ?? false'
+        @endphp
         <graphql-mutation
             v-cloak
             query="mutation subscribeNewsletter ($is_subscribed: Boolean!) { updateCustomerV2(input: { is_subscribed: $is_subscribed }) { customer { is_subscribed } } }"
             :alert="false"
             :clear="false"
-            :variables="{ is_subscribed: data?.customer?.is_subscribed ?? $root.user?.extension_attributes?.is_subscribed ?? false }"
+            :variables="{ is_subscribed: {{ $subscribedData }} }"
             v-slot="{ mutate, variables }"
         >
     @endif
 
     <x-rapidez-ct::newsletter.partials.checkbox
         :v-model="$attributes->has('v-model') ? $attributes['v-model'] : 'variables.is_subscribed'"
-        :isPartOfAnotherForm="$attributes->has('v-model')"
         :$id
     />
 
