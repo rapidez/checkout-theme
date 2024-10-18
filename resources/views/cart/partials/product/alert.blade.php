b48c3a1ec7620438ffb00815972959f86e8a2e21
Jade Geels <jade@justbetter.nl>
2024-10-18 09:40:37 +0200

    Remove unnecessary divs

--- resources/views/cart/partials/product/alert.blade.php
+++ resources/views/cart/partials/product/alert.blade.php
@@ -3,16 +3,12 @@
     v-if="item.qty_backordered"
 >
     <x-heroicon-o-exclamation-circle class="text-yellow-500 mt-px w-5" />
-    <div class="flex flex-col">
-        <div class="flex items-center gap-2">
-            <span>
-                <template v-if="item.qty_backordered < item.qty">
-                    @lang(':count of the requested quantity will be backordered', ['count' => '@{{ item.qty_backordered }}'])
-                </template>
-                <template v-else>
-                    @lang('This product will be backordered')
-                </template>
-            </span>
-        </div>
-    </div>
+    <span>
+        <template v-if="item.qty_backordered < item.qty">
+            @lang(':count of the requested quantity will be backordered', ['count' => '@{{ item.qty_backordered }}'])
+        </template>
+        <template v-else>
+            @lang('This product will be backordered')
+        </template>
+    </span>
 </div>
