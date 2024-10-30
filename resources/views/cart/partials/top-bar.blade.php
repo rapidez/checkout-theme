872220e813805a137a0ae4a34e8e7a2c5132225d
Jade Geels <jade@justbetter.nl>
2024-10-30 15:44:26 +0100

    Fix hardcoded 1/4 text

--- resources/views/cart/partials/top-bar.blade.php
+++ resources/views/cart/partials/top-bar.blade.php
@@ -1,10 +1,15 @@
+@php($checkoutSteps = config('rapidez.frontend.checkout_steps.' . config('rapidez.store_code')) ?: config('rapidez.frontend.checkout_steps.default'))
+
 <div class="flex flex-wrap justify-between items-baseline">
     <x-rapidez-ct::title tag="h1">
         @lang('Cart')
     </x-rapidez-ct::title>
     <div class="flex wrap">
         <div>
-            @lang('Step 1 of 4')
+            @lang('Step :step out of :total', [
+                'step' => 1,
+                'total' => count($checkoutSteps),
+            ])
         </div>
     </div>
 </div>
