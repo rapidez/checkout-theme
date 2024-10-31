b38c303fff454f90a4086d3e289887d4da255db1
Roene <roene@justbetter.nl>
2024-10-31 14:26:51 +0100

    Changed breakpoints for smaller screens

--- resources/views/components/layout/two-column.blade.php
+++ resources/views/components/layout/two-column.blade.php
@@ -1,6 +1,6 @@
 <div {{ $attributes->class('flex flex-col mx-auto max-w-4xl w-full') }}>
     {{ $slot }}
-    <div class="mt-5 grid content-center items-start gap-8 sm:grid-cols-2">
+    <div class="mt-5 grid content-center items-start gap-8 lg:grid-cols-2">
         {{ $columns }}
     </div>
 </div>
