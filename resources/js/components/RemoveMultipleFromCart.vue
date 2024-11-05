b41e47cf52a63d0a93d780998409ed10bbba30f6
Bob <bob@justbetter.nl>
2024-11-05 10:42:55 +0100

    Add button to remove out of stock products

---
+++ resources/js/components/RemoveMultipleFromCart.vue
@@ -0,0 +1,28 @@
+<script>
+import { cart } from 'Vendor/rapidez/core/resources/js/stores/useCart.js'
+import { mask } from 'Vendor/rapidez/core/resources/js/stores/useMask.js'
+export default {
+    render() {
+        return this.$scopedSlots.default(this)
+    },
+    methods: {
+        async remove() {
+            await Promise.all(this.outOfStockItems.map(async (item) => {
+                let response = await window.magentoGraphQL(
+                    `mutation ($cart_id: String!, $cart_item_id: Int) { removeItemFromCart(input: { cart_id: $cart_id, cart_item_id: $cart_item_id }) { cart { ...cart } } } ` + config.fragments.cart,
+                    {
+                        cart_id: mask.value,
+                        cart_item_id: item.id,
+                    },
+                )
+                this.updateCart({}, response)
+            }))
+        }
+    },
+    computed: {
+        outOfStockItems() {
+            return cart.value.items.filter((item) => !this.$root.canOrderCartItem(item))
+        }
+    }
+}
+</script>
--- resources/js/package.js
+++ resources/js/package.js
@@ -1,3 +1,4 @@
 Vue.component('checkout-address', () => import('./components/CheckoutAddress.vue'))
 Vue.component('checkout-success-addresses', () => import('./components/CheckoutSuccessAddresses.vue'))
 Vue.component('address-card', () => import('./components/AddressCard.vue'))
+Vue.component('remove-multiple-from-cart', () => import('./components/RemoveMultipleFromCart.vue'))
