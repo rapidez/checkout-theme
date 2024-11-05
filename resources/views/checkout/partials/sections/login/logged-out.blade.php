daa946d1693798c5ef97375c43fa49a416060bc2
Jordy de Vulder <jordy@justbetter.nl>
2024-11-05 14:47:16 +0100

    forgot one file

--- resources/views/checkout/partials/sections/login/logged-out.blade.php
+++ resources/views/checkout/partials/sections/login/logged-out.blade.php
@@ -1,9 +1,5 @@
 <checkout-login v-slot="checkoutLogin">
-<<<<<<< HEAD
-    <fieldset data-function="go" class="flex flex-col gap-3">
-=======
     <fieldset partial-submit="go" class="grid gap-4 md:gap-5 md:grid-cols-2">
->>>>>>> 6cc528cd1c252e24d49c24058012c98f5a281ac8
         <x-rapidez-ct::input
             label="Email"
             name="email"
@@ -14,62 +10,11 @@
             required
             :placeholder="__('Enter your e-mail address')"
         />
-<<<<<<< HEAD
-        <template v-if="!loggedIn && (!checkoutLogin.isEmailAvailable || checkoutLogin.createAccount)">
-            <x-rapidez-ct::input
-                label="Password"
-                name="password"
-                type="password"
-                v-model="checkoutLogin.password"
-                required
-            />
-        </template>
-        <p v-if="!loggedIn && !checkoutLogin.isEmailAvailable" class="self-end text-ct-inactive">
-            @lang('You already have an account with this e-mail address. Please log in to continue.')
-            <a href="{{ route('account.forgotpassword') }}" class="underline hover:no-underline">@lang('Forgot your password?')</a>
-        </p>
-        @if (App::providerIsLoaded('Rapidez\Account\AccountServiceProvider'))
-            <a href="{{ route('account.forgotpassword') }}" class="inline-block text-sm hover:underline mt-5" v-if="!checkoutLogin.isEmailAvailable">
-                @lang('Forgot your password?')
-            </a>
-        @endif
-        <template v-if="!loggedIn && checkoutLogin.createAccount">
-            <x-rapidez-ct::input
-                label="Repeat password"
-                name="password_repeat"
-                type="password"
-                v-model="checkoutLogin.password_repeat"
-                required
-            />
-            <x-rapidez-ct::input
-                label="Firstname"
-                name="firstname"
-                type="text"
-                v-model="checkoutLogin.firstname"
-                required
-            />
-            <x-rapidez-ct::input
-                label="Lastname"
-                name="lastname"
-                type="text"
-                v-model="checkoutLogin.lastname"
-                required
-            />
-        </template>
-        <template v-if="!loggedIn && checkoutLogin.isEmailAvailable">
-            <x-rapidez::checkbox v-model="checkoutLogin.createAccount" dusk="create_account">
-                @lang('Create an account')
-            </x-rapidez::checkbox>
-        </template>
-=======
->>>>>>> 6cc528cd1c252e24d49c24058012c98f5a281ac8
 
         <p v-if="checkoutLogin.isEmailAvailable" class="self-end text-ct-inactive">
             @lang('We will send your order confirmation to this e-mail address. We will also check if you already have an account so you can checkout more efficiently.')
         </p>
 
-<<<<<<< HEAD
-=======
         <template v-if="!loggedIn && (!checkoutLogin.isEmailAvailable || checkoutLogin.createAccount)">
             <x-rapidez-ct::input.password
                 label="Password"
@@ -118,7 +63,6 @@
             </div>
         </template>
 
->>>>>>> 6cc528cd1c252e24d49c24058012c98f5a281ac8
         <x-rapidez-ct::button.accent type="button" v-on:click="checkoutLogin.go" v-if="checkoutLogin.isEmailAvailable && checkoutLogin.createAccount" dusk="register">
             @lang('Register')
         </x-rapidez-ct::button.accent>
@@ -127,4 +71,3 @@
         </x-rapidez-ct::button.accent>
     </fieldset>
 </checkout-login>
-
