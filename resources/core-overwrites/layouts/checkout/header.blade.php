d31012996e23e2dacce473f0a0d7efddd0f6aef4
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2024-11-08 10:27:26 +0100

    Add default header & footer

---
+++ resources/core-overwrites/layouts/checkout/header.blade.php
@@ -0,0 +1,16 @@
+<div class="bg-inactive-100 text-ct-inactive relative py-3 text-xs font-semibold">
+    <slider reference="header-usp-slider" autoplay>
+        <div
+            ref="header-usp-slider"
+            class="scrollbar-hide flex snap-x snap-mandatory items-center gap-7 overflow-x-auto lg:justify-center"
+            slot-scope="{ navigate, currentSlide, slidesTotal }"
+        >
+            @foreach (['Wide range of products', 'Free returns', 'All items shown are in stock', 'Free shipping'] as $usp)
+                <div class="flex shrink-0 snap-center items-center justify-center gap-2 max-lg:w-screen">
+                    <x-heroicon-o-check class="size-4 text-ct-enhanced stroke-2" />
+                    <span>{{ $usp }}</span>
+                </div>
+            @endforeach
+        </div>
+    </slider>
+</div>
