8986ec6e679ef328030f24f424b761f114bb8e2a
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2024-11-12 14:50:46 +0100

    change w-screen to w-full

--- resources/core-overwrites/layouts/checkout/header.blade.php
+++ resources/core-overwrites/layouts/checkout/header.blade.php
@@ -6,7 +6,7 @@
             slot-scope="{ navigate, currentSlide, slidesTotal }"
         >
             @foreach (['Wide range of products', 'Free returns', 'All items shown are in stock', 'Free shipping'] as $usp)
-                <div class="flex shrink-0 snap-center items-center justify-center gap-2 max-lg:w-screen">
+                <div class="flex shrink-0 snap-center items-center justify-center gap-2 max-lg:w-full">
                     <x-heroicon-o-check class="size-4 text-ct-enhanced stroke-2" />
                     <span>{{ $usp }}</span>
                 </div>
