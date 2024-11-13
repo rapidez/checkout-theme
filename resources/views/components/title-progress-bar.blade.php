08b2ff52dd1d99f228b1fa687c27b15c1a731bf9
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2024-11-13 10:04:59 +0100

    Adjust alignment

--- resources/views/components/title-progress-bar.blade.php
+++ resources/views/components/title-progress-bar.blade.php
@@ -1,8 +1,8 @@
 @aware(['checkoutSteps', 'currentStep', 'currentStepKey'])
 @php($isAnchor = $attributes->filter(null)->hasAny('href', ':href', 'v-bind:href'))
-<div class="flex flex-wrap items-center justify-between gap-1">
+<div class="flex flex-wrap items-end justify-between gap-1">
     <div class="flex gap-5 max-lg:flex-col lg:-translate-x-10">
-        <div class="flex items-center gap-5">
+        <div class="flex items-center gap-5 lg:items-center">
             @if ($isAnchor)
                 <a href="{{ $attributes->get('href', ':href', 'v-bind:href') }}" class="max-lg:size-14 max-lg:flex max-lg:items-center max-lg:justify-center max-lg:rounded-full max-lg:border">
                     <x-heroicon-o-arrow-left class="text-ct-primary size-5" />
