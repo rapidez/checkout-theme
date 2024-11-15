9a40bb5e9d0ad466346e697ae1fefd80e9f58705
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2024-11-15 08:15:42 +0100

    Replace v-if check variable

--- resources/views/cart/partials/product/price.blade.php
+++ resources/views/cart/partials/product/price.blade.php
@@ -7,7 +7,7 @@
     </div>
 </td>
 <td class="flex items-center font-medium max-md:w-1/3 md:table-cell *:mx-auto">
-    <template v-if="canOrderCartItem(item)">
+    <template v-if="item.is_available">
         <x-rapidez-ct::input.quantity/>
     </template>
 </td>
