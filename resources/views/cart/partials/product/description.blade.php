50b728fc4703c9a8f68515db8a9a7ea82bb3df90
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2024-11-15 08:13:49 +0100

    Replace v-if check variable

--- resources/views/cart/partials/product/description.blade.php
+++ resources/views/cart/partials/product/description.blade.php
@@ -9,7 +9,7 @@
             <div v-for="option in item.customizable_options">
                 @{{ option.label }}: @{{ option.values[0].label || option.values[0].value }}
             </div>
-            <div v-if="!canOrderCartItem(item)" class="text-ct-error text-balance bg-ct-error/5 my-2 max-w-sm p-2">
+            <div v-if="!item.is_available" class="text-ct-error text-balance bg-ct-error/5 my-2 max-w-sm p-2">
                 @lang('This product is out of stock, remove it to continue your order.')
             </div>
             <div v-for="option in config.cart_attributes">
