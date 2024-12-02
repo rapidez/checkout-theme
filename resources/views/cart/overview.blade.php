ba0689a8bc2a0306bb9d0d92ecfb077c7ffd4641
Jade Geels <jade@justbetter.nl>
2024-12-02 13:33:02 +0100

    Fix getCart query

--- resources/views/cart/overview.blade.php
+++ resources/views/cart/overview.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <graphql v-if="mask"
-        :query="'query getCart($cart_id: String!) { cart (cart_id: $cart_id) { ' + config.queries.cart + ' } }'"
+        :query="'query getCart($cart_id: String!) { cart (cart_id: $cart_id) { ...cart } } ' + config.fragments.cart"
         :variables="{ cart_id: mask }" 
         :callback="updateCart" 
         :error-callback="checkResponseForExpiredCart"
