8b135610ea4dceda280f65183e0e8f880bb92a4d
Jade Geels <jade@justbetter.nl>
2024-12-17 10:40:51 +0000

    Refresh user data on address change (#159)

--- resources/views/account/partials/address-cards/delete.blade.php
+++ resources/views/account/partials/address-cards/delete.blade.php
@@ -1,4 +1,9 @@
-<graphql-mutation query="mutation deleteCustomerAddress($id: Int!){ deleteCustomerAddress ( id: $id ) }" :variables="{ id: userAddress.id }" v-slot="{ mutate }">
+<graphql-mutation
+    query="mutation deleteCustomerAddress($id: Int!){ deleteCustomerAddress ( id: $id ) }"
+    :variables="{ id: userAddress.id }"
+    :callback="refreshUserInfoCallback"
+    v-slot="{ mutate }"
+>
     <x-rapidez-ct::button.link v-if="!userAddress.default_billing && !userAddress.default_shipping" v-on:click="data.customer.addresses.splice(addressIndex,1);mutate()">
         @lang('Delete address')
     </x-rapidez-ct::button.link>
