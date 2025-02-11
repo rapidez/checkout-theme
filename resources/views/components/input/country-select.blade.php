3e63464c3502da574df66220a02a7e5d18167b91
Roene Verbeek <85165259+Roene-JustBetter@users.noreply.github.com>
2025-02-11 15:55:42 +0100

    Update country-select.blade.php

--- resources/views/components/input/country-select.blade.php
+++ resources/views/components/input/country-select.blade.php
@@ -2,7 +2,7 @@
 <graphql query="{ countries { two_letter_abbreviation full_name_locale } }" cache="countries">
     <template v-if="data" slot-scope="{ data }">
         <x-rapidez-ct::input.select :$label {{ $attributes }}>
-            <option v-for="country in data.countries.sort((a, b) => a.full_name_locale.localeCompare(b.full_name_locale))" :value="country.two_letter_abbreviation.toUpperCase()">
+            <option v-for="country in Object.values(data.countries).sort((a, b) => a.full_name_locale.localeCompare(b.full_name_locale))" :value="country.two_letter_abbreviation.toUpperCase()">
                 @{{ country.full_name_locale }}
             </option>
         </x-rapidez-ct::input.select>
