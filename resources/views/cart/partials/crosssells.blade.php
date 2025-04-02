5d8f90bc0df2aa64fac36175d68b10f1a706ffdd
Jade Geels <jade@justbetter.nl>
2025-04-02 13:14:03 +0200

    Fix crosssells (#171)

--- resources/views/cart/partials/crosssells.blade.php
+++ resources/views/cart/partials/crosssells.blade.php
@@ -1,3 +1,7 @@
-<div v-if="cart?.cross_sells?.length" class="mt-5">
-    <x-rapidez::productlist value="cart.cross_sells" title="Related products" field="entity_id"/>
+<div v-if="cart.items.flatMap((item) => item.product.crosssell_products.map((crosssell) => crosssell.id)).length" class="mt-5">
+    <x-rapidez::productlist
+        value="cart.items.flatMap((item) => item.product.crosssell_products.map((crosssell) => crosssell.id))"
+        title="Related products"
+        field="entity_id"
+    />
 </div>
