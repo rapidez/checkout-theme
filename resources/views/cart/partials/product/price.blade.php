2d1ed2634bd4f67d4c0b7bf193cc443ecdf6398d
Roy Duineveld <royduineveld@gmail.com>
2025-06-24 15:11:05 +0200

    Update price.blade.php

--- resources/views/cart/partials/product/price.blade.php
+++ resources/views/cart/partials/product/price.blade.php
@@ -3,7 +3,12 @@
         @{{ item.prices.specialPrice | price }}
     </div>
     <div :class="{ 'line-through text-xs text-muted font-normal': item.prices.specialPrice }">
-        @{{ item.prices.price_including_tax.value | price }}
+        <template v-if="showTax">
+            @{{ item.prices.price_including_tax.value | price }}
+        </template>
+        <template v-else>
+            @{{ item.prices.price.value | price }}
+        </template>
     </div>
 </td>
 <td class="flex items-center font-medium max-md:w-1/3 md:table-cell *:mx-auto">
@@ -12,5 +17,10 @@
     </template>
 </td>
 <td class="flex items-center justify-end text-right font-medium max-md:w-1/3 md:table-cell">
-    @{{ item.prices.row_total_including_tax.value | price }}
+    <template v-if="showTax">
+        @{{ item.prices.row_total_including_tax.value | price }}
+    </template>
+    <template v-else>
+        @{{ item.prices.row_total.value | price }}
+    </template>
 </td>
