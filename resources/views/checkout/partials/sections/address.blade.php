2127eaf99c7f0fa4a87935cf8af19160e74f97f8
Jade Geels <jade@justbetter.nl>
2025-08-05 18:12:59 +0200

    Remove duplication and bugfix (#184)

--- resources/views/checkout/partials/sections/address.blade.php
+++ resources/views/checkout/partials/sections/address.blade.php
@@ -6,49 +6,9 @@
         </div>
     </template>
     <template v-else>
-        <div>
-            <graphql-mutation
-                :query="config.queries.setNewShippingAddressesOnCart"
-                :variables="{
-                    cart_id: mask,
-                    ...window.address_defaults,
-                    ...cart.shipping_addresses[0],
-                    country_code: cart.shipping_addresses[0]?.country.code || window.address_defaults.country_code,
-                    region_id: cart.shipping_addresses[0]?.region.region_id || window.address_defaults.region_id,
-                }"
-                group="shipping"
-                :callback="updateCart"
-                :error-callback="checkResponseForExpiredCart"
-                :watch="false"
-                mutate-event="setShippingAddressesOnCart"
-                v-slot="{ mutate, variables }"
-                v-if="!cart.is_virtual"
-            >
-                <fieldset partial-submit="mutate" v-on:change="function (e) {e.target.closest('fieldset').querySelector(':invalid') === null && mutate().then(() => (cart?.billing_address?.same_as_shipping ?? true) && window.app.$emit('setBillingAddressOnCart'))}">
-                    @include('rapidez-ct::checkout.partials.shipping-billing-fields', ['type' => 'shipping'])
-                </fieldset>
-            </graphql-mutation>
-            <graphql-mutation
-                :query="config.queries.setNewBillingAddressOnCart"
-                :variables="JSON.parse(JSON.stringify({
-                    cart_id: mask,
-                    ...window.address_defaults,
-                    ...cart.billing_address,
-                    same_as_shipping: !cart.is_virtual && (cart?.billing_address?.same_as_shipping ?? true),
-                    country_code: cart.billing_address?.country.code || window.address_defaults.country_code,
-                    region_id: cart.billing_address?.region.region_id || window.address_defaults.region_id,
-                }))"
-                :callback="updateCart"
-                :error-callback="checkResponseForExpiredCart"
-                :watch="false"
-                group="billing"
-                mutate-event="setBillingAddressOnCart"
-                v-slot="{ mutate, variables }"
-            >
-                <fieldset partial-submit="mutate" v-on:change="function (e) {e.target.closest('fieldset').querySelector(':invalid') === null && mutate().then(() => (cart?.billing_address?.same_as_shipping ?? true) && window.app.$emit('setBillingAddressOnCart'))}">
-                    @include('rapidez-ct::checkout.partials.shipping-billing-fields', ['type' => 'billing'])
-                </fieldset>
-            </graphql-mutation>
+        <div class="flex flex-col gap-6">
+            @include('rapidez::checkout.steps.shipping_address')
+            @include('rapidez::checkout.steps.billing_address')
         </div>
     </template>
 </checkout-address>
--- resources/views/checkout/partials/shipping-billing-fields.blade.php
+++ resources/views/checkout/partials/shipping-billing-fields.blade.php
@@ -8,15 +8,4 @@
     </p>
 @endif
 
-
-<template @if($type == 'billing') v-if="!variables.same_as_shipping" @endif >
-    <x-rapidez-ct::address-form :$type/>
-</template>
-
-@if ($type == 'billing')
-    <div class="mt-5" v-if="!cart.is_virtual">
-        <x-rapidez::input.checkbox v-model="variables.same_as_shipping">
-            @lang('My billing and shipping address are the same')
-        </x-rapidez::input.checkbox>
-    </div>
-@endif
+<x-rapidez-ct::address-form :$type/>
