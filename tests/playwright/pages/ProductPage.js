43e4039d4268d9522433f60f153aab14a8e05a28
Roene <roene@justbetter.nl>
2025-08-29 14:37:07 +0200

    Re-add install command back | Don't make use of exports

--- tests/playwright/pages/ProductPage.js
+++ tests/playwright/pages/ProductPage.js
@@ -1,1 +1,27 @@
-export * from '../../../vendor/rapidez/core/tests/playwright/pages/ProductPage.js'
+export class ProductPage {
+    constructor(page) {
+        this.page = page
+    }
+
+    async goto(url) {
+        await this.page.goto(url)
+        return await this.page.evaluate(() => window.config.product)
+    }
+
+    async addToCart(url, qty = 1) {
+        const product = await this.goto(url)
+
+        if (qty > 1) {
+            await this.page.waitForTimeout(100)
+            await this.page.getByTestId('qty').fill('')
+            await this.page.waitForTimeout(100)
+            await this.page.getByTestId('qty').pressSequentially(qty.toString())
+        }
+
+        await this.page.getByTestId('add-to-cart').click()
+        await this.page.waitForTimeout(100)
+        await this.page.waitForLoadState('networkidle')
+
+        return product
+    }
+}
