62ae4c8f707b2e5777a97e27e21599eb4d497c4d
Roy Duineveld <royduineveld@gmail.com>
2025-09-25 15:21:01 +0200

    Cleanup

--- tests/playwright/cart.spec.js
+++ tests/playwright/cart.spec.js
@@ -1,2 +1,2 @@
-// Added this to run the same test that are in the core
-export * from '../../vendor/rapidez/core/tests/playwright/cart.spec.js'
+// Run the cart tests from the core
+import '../../vendor/rapidez/core/tests/playwright/cart.spec.js'
--- tests/playwright/pages/CartPage.js
+++
@@ -1,2 +0,0 @@
-// Added this to run the same test that are in the core
-export * from '../../../vendor/rapidez/core/tests/playwright/pages/CartPage.js'
