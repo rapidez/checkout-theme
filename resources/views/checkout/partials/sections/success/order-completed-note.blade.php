4032c323f2f7d9cea7a897e70056b70ddebf7ffb
Jade Geels <jade@justbetter.nl>
2025-09-30 10:33:53 +0200

    Success page login required to view order fallback (#194)

--- resources/views/checkout/partials/sections/success/order-completed-note.blade.php
+++ resources/views/checkout/partials/sections/success/order-completed-note.blade.php
@@ -4,4 +4,10 @@
 <p class="mt-4 text-sm">
     @lang('We will send a confirmation of your order :orderid to :email', ['orderid' => '@{{ order.number }}', 'email' => '@{{ order.email }}'])
 </p>
-<p class="flex flex-wrap items-center gap-x-1 mt-4 text-sm">@lang('Your order is currently'): <strong v-blur>@{{ order.status }}</strong> <a class="inline-block" href="#" v-on:click.prevent="(e) => {e.target.classList.add('animate-spin'); refreshOrder().finally(() => e.target.classList.remove('animate-spin'))}"><x-heroicon-o-arrow-path class="size-4" /></a></p>
+
+<template v-if="needsLogin">
+    <p class="mt-4 text-sm">@lang('Please log in to view the status of your order')</p>
+</template>
+<template v-else>
+    <p class="mt-4 text-sm">@lang('Your order is currently'): <span class="font-bold" v-blur>@{{ order.status }}</span> <a class="inline-block" href="#" v-on:click.prevent="(e) => {e.target.classList.add('animate-spin'); refreshOrder().finally(() => e.target.classList.remove('animate-spin'))}"><x-heroicon-o-arrow-path class="size-4" /></a></p>
+</template>
