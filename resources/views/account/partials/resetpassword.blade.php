1e704d45f0f253485813c59616bb5451c14f8deb
Roene Verbeek <85165259+Roene-JustBetter@users.noreply.github.com>
2025-09-30 16:09:46 +0200

    Hide token and prefill email in reset password form (#202)

--- resources/views/account/partials/resetpassword.blade.php
+++ resources/views/account/partials/resetpassword.blade.php
@@ -1,12 +1,13 @@
 <graphql-mutation
     v-cloak
     query="mutation reset($email: String!, $token: String!, $password: String!) { resetPassword ( email: $email, resetPasswordToken: $token, newPassword: $password ) }"
-    :variables="{ token: '{{ request()->token }}' }"
+    :variables="{ token: '{{ request()->token }}', email: '{{ request()->email }}' }"
     :clear="true"
     :notify="{ message: '@lang('Your password has been changed, please login.')' }"
+    redirect="{{ route('account.login') }}"
 >
     <form class="flex flex-col gap-y-5" v-on:submit.prevent="mutate" slot-scope="{ mutate, variables }">
-        <label>
+        <label class="hidden">
             <x-rapidez::label>@lang('Security token')</x-rapidez::label>
             <x-rapidez::input
                 name="token"
