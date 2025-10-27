422b778f79c832b8b9a152054c0fa12208cca21c
indykoning <15870933+indykoning@users.noreply.github.com>
2025-10-27 09:56:28 +0100

    Added notifications component back into checkout footer

--- resources/core-overwrites/layouts/checkout/footer.blade.php
+++ resources/core-overwrites/layouts/checkout/footer.blade.php
@@ -1,3 +1,4 @@
+<x-rapidez::notifications />
 <footer class="mt-auto">
     <div class="mt-10 flex items-center justify-center gap-6 border-b-4 border-b-primary bg py-4 text-sm text-muted">
         @foreach (['Privacy Policy', 'Terms & Conditions'] as $link)
