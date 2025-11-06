423949154e82cf16ee043fb091ececba0d62b22a
Jade <jade@justbetter.nl>
2025-11-06 09:03:20 +0100

    Fix address cards not changing

--- resources/views/account/partials/address-cards/buttons.blade.php
+++ resources/views/account/partials/address-cards/buttons.blade.php
@@ -1,19 +1,19 @@
 @include('rapidez-ct::account.partials.address-cards.delete')
 
-<x-rapidez-ct::button.link v-bind:href="`/account/address/${userAddress.id}`" data-testid="address-edit">
+<x-rapidez-ct::button.link v-bind:href="`/account/address/${variables.id}`" data-testid="address-edit">
     @lang('Edit')
 </x-rapidez-ct::button.link>
 
-<x-rapidez-ct::button.link v-bind:disabled="mutating" v-if="!userAddress.default_shipping" v-on:click="userAddress.default_shipping=true;mutate()">
+<x-rapidez-ct::button.link v-bind:disabled="mutating" v-if="!variables.default_shipping" v-on:click="variables.default_shipping=true;mutate()">
     @lang('Select as shipping')
 </x-rapidez-ct::button.link>
-<x-rapidez-ct::button.link v-bind:disabled="mutating" v-else v-on:click="userAddress.default_shipping=false;mutate()">
+<x-rapidez-ct::button.link v-bind:disabled="mutating" v-else v-on:click="variables.default_shipping=false;mutate()">
     @lang('Deselect as shipping')
 </x-rapidez-ct::button.link>
 
-<x-rapidez-ct::button.link v-bind:disabled="mutating" v-if="!userAddress.default_billing" v-on:click="userAddress.default_billing=true;mutate()">
+<x-rapidez-ct::button.link v-bind:disabled="mutating" v-if="!variables.default_billing" v-on:click="variables.default_billing=true;mutate()">
     @lang('Select as billing')
 </x-rapidez-ct::button.link>
-<x-rapidez-ct::button.link v-bind:disabled="mutating" v-else v-on:click="userAddress.default_billing=false;mutate()">
+<x-rapidez-ct::button.link v-bind:disabled="mutating" v-else v-on:click="variables.default_billing=false;mutate()">
     @lang('Deselect as billing')
 </x-rapidez-ct::button.link>
