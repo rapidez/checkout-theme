6e5ddec2f720e4dbae07a4c629e548005006082d
Jade Geels <jade@justbetter.nl>
2026-07-08 13:27:51 +0200

    Update register-account.blade.php

--- resources/views/account/partials/register-account.blade.php
+++ resources/views/account/partials/register-account.blade.php
@@ -92,7 +92,7 @@
                                         name="taxvat"
                                         type="text"
                                         v-model="variables.taxvat"
-                                        v-on:change="window.$emit('vat-change', $event)"
+                                        v-validate.vat="true"
                                         required
                                     />
                                 </label>
